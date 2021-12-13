ac1fe8d828135e2ff626e082b08b7a0495844230
niry1 <niry.hoareau@gmail.com>
2021-12-13 20:26:39 +0100

    Scrapping title date pub and resume and save on list structured data

--- crawler.py
+++ crawler.py
@@ -10,3 +10,29 @@ html = bs(page, 'html')
 titre = html.findAll('td', {'class': 'jobLabel'})
 
 for element in titre: print(element.get_text())
+
+allJobs = html.findAll('div', class_='job')
+titre_offre = []
+date_offre = []
+resume_offre = []
+
+for job in allJobs:
+  for titles in job.findAll('td', {'class': 'jobLabel'}):
+    title = titles.get_text()
+    titre_offre.append(title)
+    
+  datepubs = job.findAll('td', {'class': 'jobPublished'})
+  date = datepubs[0].get_text()
+  date_offre.append(date)
+  
+  for resumes in job.findAll('td', {'class': 'jobResume'}):
+    resume = resumes.get_text()
+    resume_offre.append(resume) 
+
+#Print all Titles Jobs 
+print(titre_offre) 
+
+#Print all Date Pub 
+print(date_offre) 
+#Print all Resume 
+print(resume_offre)
