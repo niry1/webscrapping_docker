e1334160caab1914cfe46bec5f6e556b24eb8a91
niry1 <niry.hoareau@gmail.com>
2021-12-13 22:07:02 +0100

    missing .parser in html

--- crawler.py
+++ crawler.py
@@ -1,33 +1,33 @@
-#!/usr/local/bin/python3
-
 from bs4 import BeautifulSoup as bs 
-from urllib.request import Request,urlopen
+from urllib.request import Request, urlopen
+import pandas as pd
 
 url = "https://www.sfds.asso.fr/fr/n/506-consulter_les_offres_demploi/?jedu=MASTER&jcon=&jp=3"
 page = urlopen(url)
-html = bs(page, 'html')
+html = bs(page, 'html.parser')
 
 titre = html.findAll('td', {'class': 'jobLabel'})
 
 for element in titre: print(element.get_text())
 
 allJobs = html.findAll('div', class_='job')
+
 titre_offre = []
 date_offre = []
 resume_offre = []
 
 for job in allJobs:
-  for titles in job.findAll('td', {'class': 'jobLabel'}):
-    title = titles.get_text()
-    titre_offre.append(title)
+    for titles in job.findAll('td', {'class': 'jobLabel'}):
+        title = titles.get_text()
+        titre_offre.append(title)
     
-  datepubs = job.findAll('td', {'class': 'jobPublished'})
-  date = datepubs[0].get_text()
-  date_offre.append(date)
+    datepubs = job.findAll('td', {'class': 'jobPublished'})
+    date = datepubs[0].get_text()
+    date_offre.append(date)
   
-  for resumes in job.findAll('td', {'class': 'jobResume'}):
-    resume = resumes.get_text()
-    resume_offre.append(resume) 
+    for resumes in job.findAll('td', {'class': 'jobResume'}):
+        resume = resumes.get_text()
+        resume_offre.append(resume) 
 
 #Print all Titles Jobs 
 #print(titre_offre) 
@@ -36,7 +36,6 @@ for job in allJobs:
 #Print all Resume 
 #print(resume_offre)
 
-import pandas as pd
 dfJobs = pd.DataFrame({
   'TITRE_OFFRE': titre_offre,
   'DATE_PUB_OFFRE': date_offre,
